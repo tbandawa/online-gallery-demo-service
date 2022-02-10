420554c4066ddb6939fb51630da7b63bb34b706e
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-02-11 00:16:51 +0200

    push source to main

---
+++ src/test/java/me/tbandawa/images/SpringImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tbandawa.images;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
