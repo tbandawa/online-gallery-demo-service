c8c45bf28ac8726cd64d7f6edfa48476365120f5
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-02-12 03:39:28 +0200

    create gallery interface

---
+++ src/main/java/me/tbandawa/api/gallery/services/GalleryService.java
@@ -0,0 +1,18 @@
+package me.tbandawa.api.gallery.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import me.tbandawa.api.gallery.models.Gallery;
+
+public interface GalleryService {
+	
+	Gallery saveGallery(Gallery gallery);
+	
+	List<Gallery> getAllGallery();
+	
+	Optional<Gallery> getGallery(long id);
+	
+	void deleteGallery(long id);
+
+}
