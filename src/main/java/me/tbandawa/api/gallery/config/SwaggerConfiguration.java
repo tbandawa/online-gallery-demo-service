651631c8f735ae1245dfd7d63f8afba6a46224bc
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-02-14 20:19:34 +0200

    add springdoc open api

--- src/main/java/me/tbandawa/api/gallery/config/SwaggerConfiguration.java
+++ src/main/java/me/tbandawa/api/gallery/config/SwaggerConfiguration.java
@@ -1,38 +1,5 @@
 package me.tbandawa.api.gallery.config;
 
-import org.springframework.context.annotation.Bean;
-
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
-
-@EnableSwagger2
 public class SwaggerConfiguration {
-	
-	@Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("me.tbandawa.api.gallery"))
-                .paths(regex("/api.*"))
-                .build()
-                .apiInfo(metaData());
-    }
-	
-    private ApiInfo metaData() {
-        ApiInfo apiInfo = new ApiInfo(
-                "Spring Boot REST API",
-                "Spring Boot REST API for Uploading Images",
-                "1.0",
-                "Terms of service",
-                new Contact("Tendai bandawa", "https://github.com/tbandawa", "tonderaibandawa@gmail.com"),
-               "Apache License Version 2.0",
-                "https://www.apache.org/licenses/LICENSE-2.0");
-        return apiInfo;
-    }
 
 }
