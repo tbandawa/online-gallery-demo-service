3de51c97f76692a3380b61fef3d7fdf9c862ed38
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-02-14 12:46:58 +0200

    comment ImageServiceImpl class

--- src/main/java/me/tbandawa/api/gallery/services/ImageServiceImpl.java
+++ src/main/java/me/tbandawa/api/gallery/services/ImageServiceImpl.java
@@ -21,6 +21,9 @@ import me.tbandawa.api.gallery.exceptions.FileStorageException;
 import me.tbandawa.api.gallery.exceptions.InvalidFileTypeException;
 import me.tbandawa.api.gallery.props.FolderProperties;
 
+/**
+ * Defines image file operations.
+ */
 @Service
 public class ImageServiceImpl implements ImageService {
 	
@@ -32,20 +35,34 @@ public class ImageServiceImpl implements ImageService {
 		this.folderProperties = folderProperties;
 	}
 
+	/**
+	 * Iterate through images, check if image is valid
+	 * and save to a directory.
+	 * @param galleryId directory name
+	 * @return images list of images
+	 */
 	@Override
 	public List<String> saveImages(Long gelleryId, MultipartFile[] images) {
+		// Iterate through images and check if its a valid image file
 		for (MultipartFile image : images)
 			if(!imageTypes.contains(image.getContentType()))
-				throw new InvalidFileTypeException(image.getOriginalFilename() + " is not a valid image.");	
+				throw new InvalidFileTypeException(image.getOriginalFilename() + " is not a valid image.");
+		// For save each image and return its URI relative to the server
 		return IntStream.range(0, images.length)
 		         .mapToObj(i -> saveImage(gelleryId, (i + 1), images[i]))
 		         .collect(Collectors.toList());
 	}
 
+	/**
+	 * Get folder contents as a list of URIs.
+	 * @param galleryId directory name
+	 * @return list of URIs
+	 */
 	@Override
 	public List<String> getImages(Long galleryId) {
 		List<String> imageURIs;
 		try {
+		// Iterate folder and return its content as a list of URIs
 			imageURIs = Files.list(Paths.get(folderProperties.getImagesFolder() + File.separatorChar + String.valueOf(galleryId) + File.separatorChar))
 		            .map(Path::toFile)
 		            .map(File::getPath)
@@ -61,23 +78,40 @@ public class ImageServiceImpl implements ImageService {
 		return imageURIs;
 	}
 
+	/**
+	 * Delete recursively directory.
+	 * @param galleryId folder name
+	 */
 	@Override
 	public void deleteImages(Long galleryId) {
+		// Delete image folder using Spring's file utilities
 		FileSystemUtils.deleteRecursively(new File(folderProperties.getImagesFolder() + File.separatorChar + String.valueOf(galleryId)));
 	}
 	
+	/**
+	 * Save image in the folder created and named using gallerId. The
+	 * image name is created by appending <b>imageIndex</b> to <b>image_</b>.
+	 * @param galleryId id of the created gallery
+	 * @param imageIndex index position of the image
+	 * @param image file to save
+	 * @return URI of the saved image
+	 */
 	private String saveImage(Long galleryId, int imageIndex, MultipartFile image) {		
 		String imageUri;
+		// Build image path using galleryId
 		Path imageLocation = Paths
 				.get(folderProperties.getImagesFolder() + File.separatorChar + String.valueOf(galleryId) + File.separatorChar)
 				.toAbsolutePath()
 				.normalize();
+		// Create image destination folder
 		try {
             Files.createDirectories(imageLocation);
         } catch (Exception ex) {
             throw new FileStorageException("Could not create the directory.", ex);
         }
+		// Name file by appending imageIndex to "image_"
 		String fileName = "image_" + imageIndex + "." + image.getOriginalFilename().split("\\.")[1];
+		// Move image into created destination and generate its URI
         try {
             Path targetLocation = imageLocation.resolve(fileName);
             Files.copy(image.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
@@ -86,7 +120,6 @@ public class ImageServiceImpl implements ImageService {
                     .path(folderProperties.getImagesFolder() + File.separatorChar + String.valueOf(galleryId) + File.separatorChar)
                     .path(fileName)
                     .toUriString();
-            
         } catch (IOException ex) {
             throw new FileStorageException("Could not store file " + fileName, ex);
         }
