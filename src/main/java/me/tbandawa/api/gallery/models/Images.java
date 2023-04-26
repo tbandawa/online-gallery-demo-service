c44b8ed05646268009322e5398cccf57c63b242b
Tendai  Bandawa <tonderaibandawa@gmail.com>
2023-04-26 05:47:50 +0200

    rm extra line

---
+++ src/main/java/me/tbandawa/api/gallery/models/Images.java
@@ -0,0 +1,24 @@
+package me.tbandawa.api.gallery.models;
+
+public class Images {
+	
+	private String thumbnail;
+	private String image;
+	
+	public String getThumbnail() {
+		return thumbnail;
+	}
+	
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+	
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+}
