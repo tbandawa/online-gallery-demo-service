d97580c12e202f852d55b792dd85710143e42d2b
Tendai  Bandawa <tonderaibandawa@gmail.com>
2023-05-15 13:38:22 +0200

    allow springdoc in security

--- src/main/java/me/tbandawa/api/gallery/config/WebSecurityConfig.java
+++ src/main/java/me/tbandawa/api/gallery/config/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig {
     http.cors().and().csrf().disable()
         .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-        .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+        .authorizeRequests().antMatchers("/api/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/webjars/swagger-ui/**").permitAll()
         .anyRequest().authenticated();
     
     http.authenticationProvider(authenticationProvider());
