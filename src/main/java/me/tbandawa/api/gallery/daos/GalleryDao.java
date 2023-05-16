d861c5fdf6bd6278afaf7646ce518e5c29c33b9a
Tendai  Bandawa <tonderaibandawa@gmail.com>
2023-05-16 17:11:34 +0200

    add search to dao interface

--- src/main/java/me/tbandawa/api/gallery/daos/GalleryDao.java
+++ src/main/java/me/tbandawa/api/gallery/daos/GalleryDao.java
@@ -6,12 +6,9 @@ import java.util.Optional;
 import me.tbandawa.api.gallery.entities.Gallery;
 
 public interface GalleryDao {
-	
 	Optional<Gallery> get(long id);
-
     List<Gallery> getAll();
-    
+    List<Gallery> searchGallery(String query);
     Gallery save(Gallery gallery);
-    
     void delete(long id);
 }
