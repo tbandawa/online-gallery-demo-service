0ad8fad62a946331f4781343e8cf1bc3272b14cb
Tendai  Bandawa <tonderaibandawa@gmail.com>
2023-10-18 20:44:33 +0200

    add logout controller

--- src/main/java/me/tbandawa/api/gallery/controllers/UserController.java
+++ src/main/java/me/tbandawa/api/gallery/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,7 @@ import me.tbandawa.api.gallery.entities.Images;
 import me.tbandawa.api.gallery.requests.LoginRequest;
 import me.tbandawa.api.gallery.requests.RegisterRequest;
 import me.tbandawa.api.gallery.responses.AuthResponse;
+import me.tbandawa.api.gallery.responses.LogoutResponse;
 import me.tbandawa.api.gallery.responses.UserResponse;
 import me.tbandawa.api.gallery.services.ImageService;
 import me.tbandawa.api.gallery.services.UserDetailsImpl;
@@ -92,5 +95,14 @@ public class UserController {
 	@GetMapping("/user/{id}")
     public ResponseEntity<UserResponse> getProfile(@PathVariable Long id) {
         return ResponseEntity.ok().body(userService.getUserProfile(id));
+    }
+	
+	@Operation(summary = "log out user", description = "log out user and expire token", tags = { "user" })
+	@SecurityRequirement(name = "Bearer Authentication")
+	@GetMapping("/logout")
+    public ResponseEntity<LogoutResponse> logoutUser(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+		LogoutResponse logoutResponse = new LogoutResponse();
+		logoutResponse.setToken(userService.logoutUser(token));
+		return ResponseEntity.ok().body(logoutResponse);
     }
 }
